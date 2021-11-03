77d9f47ff425ff07e9d3d7d0cbb4681f43f6d591
woodi <woodi.daily@gmail.com>
2021-11-03 09:35:21 +0900

    Apply Outer Function

--- ImageProcessingGeometry/ImageProcessingGeometry/main.cpp
+++ ImageProcessingGeometry/ImageProcessingGeometry/main.cpp
@@ -21,25 +21,37 @@ void setLabel(Mat& image, string str, vector<Point> contour) {
   putText(image, str, pt, fontface, scale, CV_RGB(0, 0, 0), thickness, 8);
 }
 
+void convertColorToGray(Mat& image, Mat& gray) {
+  cvtColor(image, gray, CV_BGR2GRAY);
+}
+
+void convertGrayToBinary(Mat& gray, Mat& binary) {
+  threshold(gray, binary, 0, 255, THRESH_BINARY | THRESH_OTSU);
+}
+
+void findContoursFromBinary(Mat& binary, vector<vector<Point> >& contours) {
+  findContours(binary, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+}
+
 int main(int, char**) {
-  Mat img_input, img_result, img_gray;
+  Mat img, img_result, img_gray;
   // Image Load
-  img_input = imread("1.png", IMREAD_COLOR);
-  if (img_input.empty()) {
+  img = imread("1.png", IMREAD_COLOR);
+  if (img.empty()) {
     cout << "No Image Found\n";
     return -1;
   }
+
   // Convert Image to GrayScale
-  cvtColor(img_input, img_gray, COLOR_BGR2GRAY);
+  convertColorToGray(img, img_gray);
   // Convert Image to Binary Image
-  Mat binary_image;
-  threshold(img_gray, img_gray, 0, 255, THRESH_BINARY_INV | THRESH_OTSU);
+  convertGrayToBinary(img_gray, img_gray);
   // Find Contours of Images
   vector<vector<Point> > contours;
-  findContours(img_gray, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
+  findContoursFromBinary(img_gray, contours);
   // Approximate Contours
   vector<Point2f> approx;
-  img_result = img_input.clone();
+  img_result = img.clone();
   for (size_t i = 0; i < contours.size(); i++) {
     approxPolyDP(Mat(contours[i]), approx,
                  arcLength(Mat(contours[i]), true) * 0.02, true);
@@ -47,22 +59,22 @@ int main(int, char**) {
     {
       int size = approx.size();
       // Decide whether the contour is a triangle or a rectangle
-      if (size % 2 == 0) {
-        line(img_result, approx[0], approx[approx.size() - 1],
-             Scalar(0, 255, 0), 3);
-        for (int k = 0; k < size - 1; k++)
-          line(img_result, approx[k], approx[k + 1], Scalar(0, 255, 0), 3);
-        for (int k = 0; k < size; k++)
-          circle(img_result, approx[k], 3, Scalar(0, 0, 255));
-      } else {
-        line(img_result, approx[0], approx[approx.size() - 1],
-             Scalar(0, 255, 0), 3);
-        for (int k = 0; k < size - 1; k++)
-          line(img_result, approx[k], approx[k + 1], Scalar(0, 255, 0), 3);
+      // if (size % 2 == 0) {
+      //   line(img_result, approx[0], approx[approx.size() - 1],
+      //        Scalar(0, 255, 0), 3);
+      //   for (int k = 0; k < size - 1; k++)
+      //     line(img_result, approx[k], approx[k + 1], Scalar(0, 255, 0), 3);
+      //   for (int k = 0; k < size; k++)
+      //     circle(img_result, approx[k], 3, Scalar(0, 0, 255));
+      // } else {
+      //   line(img_result, approx[0], approx[approx.size() - 1],
+      //        Scalar(0, 255, 0), 3);
+      //   for (int k = 0; k < size - 1; k++)
+      //     line(img_result, approx[k], approx[k + 1], Scalar(0, 255, 0), 3);
 
-        for (int k = 0; k < size; k++)
-          circle(img_result, approx[k], 3, Scalar(0, 0, 255));
-      }
+      //   for (int k = 0; k < size; k++)
+      //     circle(img_result, approx[k], 3, Scalar(0, 0, 255));
+      // }
       // If approximated Contour size is over 3, it is a triangle
       if (size == 3) setLabel(img_result, "triangle", contours[i]);
       // If approximated Contour size is over 4, it is a rectangle
@@ -79,7 +91,7 @@ int main(int, char**) {
         setLabel(img_result, to_string(approx.size()), contours[i]);
     }
   }
-  imshow("input", img_input);
+  imshow("input", img);
   imshow("result", img_result);
   waitKey(0);
   return 0;
