1d55274ebdf67ec2a2c1056155dd8664b6458b8c
woodi <woodi.daily@gmail.com>
2021-11-03 14:00:50 +0900

    Finish Assignment

--- ImageProcessingGeometry/ImageProcessingGeometry/main.cpp
+++ ImageProcessingGeometry/ImageProcessingGeometry/main.cpp
@@ -33,15 +33,16 @@ void convertColorToGray(Mat& image, Mat& gray) {
     uchar* pointer_row_gray = gray.ptr<uchar>(row);
     for (int col = 0; col < cols; col++) {
       pointer_row_gray[col] =
-          (uchar)(0.299 * pointer_row[0] + 0.587 * pointer_row[1] +
-                  0.114 * pointer_row[2]);
+          (uchar)(0.299f * pointer_row[0] + 0.587f * pointer_row[1] +
+                  0.114f * pointer_row[2]);
       pointer_row += 3;
     }
   }
 }
 
-void convertGrayToBinary(Mat& gray, Mat& binary, int min, int max) {
-  int rows = gray.rows, cols = gray.cols;
+void convertGrayToBinary(Mat& gray, Mat& binary, int thresholdValue, int maxValue) {
+    /*
+      int rows = gray.rows, cols = gray.cols;
   binary.create(gray.size(), CV_8UC1);
   if (gray.isContinuous() && binary.isContinuous()) {
     cols = rows * cols;
@@ -51,14 +52,17 @@ void convertGrayToBinary(Mat& gray, Mat& binary, int min, int max) {
     uchar* pointer_row = gray.ptr<uchar>(row);
     uchar* pointer_row_binary = binary.ptr<uchar>(row);
     for (int col = 0; col < cols; col++) {
-      if (pointer_row[col] > 0 && pointer_row[col] < min) {
-        pointer_row_binary[col] = max;
+      if (pointer_row[col] > threshold) {
+        pointer_row_binary[col] = maxValue;
       } else {
         pointer_row_binary[col] = 0;
       }
     }
   }
-  // threshold(gray, binary, 0, 255, THRESH_BINARY | THRESH_OTSU);
+    */
+
+    
+   threshold(gray, binary, 0, 255, THRESH_BINARY_INV | THRESH_OTSU);
 }
 
 void findContoursFromBinary(Mat& binary, vector<vector<Point> >& contours) {
@@ -68,59 +72,38 @@ void findContoursFromBinary(Mat& binary, vector<vector<Point> >& contours) {
 int main(int, char**) {
   Mat img, img_result, img_gray;
   // Image Load
-  img = imread("1.png", IMREAD_COLOR);
+  img = imread("2.png", IMREAD_COLOR);
   if (img.empty()) {
     cout << "No Image Found\n";
     return -1;
   }
-
   // Convert Image to GrayScale
   convertColorToGray(img, img_gray);
   // Convert Image to Binary Image
-  convertGrayToBinary(img_gray, img_gray, 40, 200);
+  Mat img_binary;
+  convertGrayToBinary(img_gray, img_binary, 30, 255);
   // Find Contours of Images
   vector<vector<Point> > contours;
-  findContoursFromBinary(img_gray, contours);
+  findContoursFromBinary(img_binary, contours);
   // Approximate Contours
   vector<Point2f> approx;
   img_result = img.clone();
   for (size_t i = 0; i < contours.size(); i++) {
     approxPolyDP(Mat(contours[i]), approx,
                  arcLength(Mat(contours[i]), true) * 0.02, true);
-    if (fabs(contourArea(Mat(approx))) > 100)  // Detect Image with Area > 100)
+    if (fabs(contourArea(Mat(approx))) < 120000)  // Detect Image with Area < 120000)
     {
       int size = approx.size();
-      // Decide whether the contour is a triangle or a rectangle
-      if (size % 2 == 0) {
-        line(img_result, approx[0], approx[approx.size() - 1],
-             Scalar(0, 255, 0), 3);
-        for (int k = 0; k < size - 1; k++)
-          line(img_result, approx[k], approx[k + 1], Scalar(0, 255, 0), 3);
-        for (int k = 0; k < size; k++)
-          circle(img_result, approx[k], 3, Scalar(0, 0, 255));
-      } else {
-        line(img_result, approx[0], approx[approx.size() - 1],
-             Scalar(0, 255, 0), 3);
-        for (int k = 0; k < size - 1; k++)
-          line(img_result, approx[k], approx[k + 1], Scalar(0, 255, 0), 3);
-
-        for (int k = 0; k < size; k++)
-          circle(img_result, approx[k], 3, Scalar(0, 0, 255));
-      }
       // If approximated Contour size is over 3, it is a triangle
       if (size == 3) setLabel(img_result, "triangle", contours[i]);
       // If approximated Contour size is over 4, it is a rectangle
       else if (size == 4 && isContourConvex(Mat(approx)))
-        setLabel(img_result, "rectangle", contours[i]);
-      // If approximated Contour size is over 5, it is a pentagon
-      else if (size == 5 && isContourConvex(Mat(approx)))
-        setLabel(img_result, "pentagon", contours[i]);
-      // If approximated Contour size is over 6, it is a hexagon
-      else if (size == 6 && isContourConvex(Mat(approx)))
-        setLabel(img_result, "hexagon", contours[i]);
-      // Else Just Show the Num of Edges
+          setLabel(img_result, "rectangle", contours[i]);
+      else if (size <= 7 && isContourConvex(Mat(approx)))
+          setLabel(img_result, to_string(approx.size()), contours[i]);
+      // Else decide it is circle
       else
-        setLabel(img_result, to_string(approx.size()), contours[i]);
+        setLabel(img_result, "circle", contours[i]);
     }
   }
   imshow("input", img);
